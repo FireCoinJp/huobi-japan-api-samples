2061d20a220c79dda582b6e72a595358514dc3a3
wang.mengtong <>
2021-10-05 19:45:07 +0900

    取引ペア情報と対応取引通貨 のapi テスト完成しました

---
+++ cmds/currencys.go
@@ -0,0 +1,52 @@
+package cmds
+
+import (
+	"context"
+	"flag"
+	"huobi-japan-api-samples/config"
+	"huobi-japan-api-samples/core/api"
+	"net/http"
+
+	"github.com/google/subcommands"
+)
+
+type CurrencysCmd struct {
+	isSave bool
+}
+
+func (a *CurrencysCmd) Name() string {
+	return "currencys"
+}
+
+func (a *CurrencysCmd) Synopsis() string {
+	return "CurrencysCmd"
+}
+
+func (a *CurrencysCmd) Usage() string {
+	return "api-test currencys -save"
+}
+
+func (a *CurrencysCmd) SetFlags(set *flag.FlagSet) {
+	set.BoolVar(&a.isSave, "save", false, "write to json")
+	return
+}
+
+func (a *CurrencysCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interface{}) subcommands.ExitStatus {
+	h := api.New(config.Cfg)
+	req, _ := http.NewRequest(http.MethodGet, h.Url("/v1/common/currencys"), nil)
+	err := h.Auth(req)
+	if err != nil {
+		panic(err)
+	}
+
+	if a.isSave {
+		err = h.Do(req, api.SaveMsg)
+	} else {
+		err = h.Do(req, api.PrintMsg)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	return 0
+}
--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"context"
 	"flag"
-	"github.com/google/subcommands"
 	"huobi-japan-api-samples/cmds"
 	"huobi-japan-api-samples/config"
 	"os"
+
+	"github.com/google/subcommands"
 )
 
 func init() {
 	pwd, _ := os.Getwd()
 	var err error
-	config.Cfg, err = config.Load(pwd+"/config.yaml")
+	config.Cfg, err = config.Load(pwd + "/config.yaml")
 	if err != nil {
 		panic(err)
 	}
@@ -24,6 +25,8 @@ func main() {
 	subcommands.Register(subcommands.FlagsCommand(), "")
 	subcommands.Register(subcommands.CommandsCommand(), "")
 	subcommands.Register(&cmds.AccountsCmd{}, "")
+	subcommands.Register(&cmds.CurrencysCmd{}, "")
+	subcommands.Register(&cmds.SymbolsCmd{}, "")
 
 	flag.Parse()
 	ctx := context.Background()
